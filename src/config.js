3d45fbb8da96cb94413fc767558b2b2086b4aca0
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-12 23:13:09 -0700

    first

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 8002,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || '4d108335-91e0-47c2-bdf0-f2f4569bccd8',
+}
