2a382ddcd02198d62f77b6133d3cad7a573079a0
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-18 22:21:02 -0700

    server added

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8002,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || '4d108335-91e0-47c2-bdf0-f2f4569bccd8',
+    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
